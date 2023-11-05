fee87391b442bd71ebeb50d4d69d9f9156b60ed1
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2023-11-05 20:56:57 +0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      CV App
+    </>
+  )
+}
+
+export default App
